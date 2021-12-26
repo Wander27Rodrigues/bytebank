b52884dbbab417bce68a000df93104e6b5c88087
wander rodrigues <wanderrodrigues_@outlook.com>
2021-12-26 12:41:12 -0300

    refatorando styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NovaTransferenciaComponent } from './nova-tranferencia/novatranferencia/novatransferencia.component';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { NovaTransferenciaComponent } from './nova-tranferencia/novatranferencia/novatransferencia.component';
+import localePt from '@angular/common/locales/pt';
+
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -18,10 +24,16 @@ import { ExtratoComponent } from './extrato/extrato.component';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule
 
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
